a31f6b295243da42e571ac312102a018b1fbedad
omarsha157 <omarsha157@gmail.com>
2022-08-16 08:30:20 +0530

    added new project to more-projects

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
@@ -21,7 +23,6 @@ const navSlide = () => {
             } else {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
             }
-
         })
     })
 
@@ -36,8 +37,8 @@ const navClose = () => {
     const wrapper = document.querySelector('.wrapper')
     const navLinksA = document.querySelectorAll('.nav-links li a');
 
-    for (let i = 0; i < navLinksA.length; i++) {
-        navLinksA[i].addEventListener('click', () => {
+    navLinksA.forEach(links => 
+        links.addEventListener('click', () => {
             nav.classList.remove('nav-active')
             burger.classList.remove('burger-close')
             wrapper.classList.remove('wrapper-blur')
@@ -48,14 +49,38 @@ const navClose = () => {
                 } else {
                     link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
                 }
-
             })
         })
-    }
-
-
-
+    )
 }
+
+// same code using for loop instead of foreach
+
+// const navClose = () => {
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('.nav-links');
+//     const navLinks = document.querySelectorAll('.nav-links li');
+//     const wrapper = document.querySelector('.wrapper')
+//     const navLinksA = document.querySelectorAll('.nav-links li a');
+
+//     for (let i = 0; i < navLinksA.length; i++) {
+//         navLinksA[i].addEventListener('click', () => {
+//             nav.classList.remove('nav-active')
+//             burger.classList.remove('burger-close')
+//             wrapper.classList.remove('wrapper-blur')
+
+//             navLinks.forEach((link, index) => {
+//                 if (link.style.animation) {
+//                     link.style.animation = '';
+//                 } else {
+//                     link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+//                 }
+
+//             })
+//         })
+//     }
+// }
+
 
 const scrollPadding = () => {
     const navigationHeight = document.querySelector('nav').offsetHeight;
